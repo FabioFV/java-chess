35fa4250706e7529b89b9971dd96e79d0cfe901b
Fabio Ferreras <fabio_ferreras@hotmail.com>
2016-07-09 21:25:59 -0400

    Implementing grid system with click listener

--- src/controller/MainGameController.java
+++ src/controller/MainGameController.java
@@ -1,7 +1,39 @@
 package controller;
 
-public class MainGameController {
+import view.MainGame;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+public class MainGameController implements MouseListener{
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Point p = e.getPoint();
+        int x = (int) p.getX() / 60;
+        int y = (int) ((p.getY() + 30) / 60) - 1;
+        System.out.println("["+x+","+y+"]");
+        MainGame.clickTail(new Point(x,y));
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
 }
--- src/view/MainGame.java
+++ src/view/MainGame.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.MainGameController;
 import model.Board;
 
 import javax.swing.*;
@@ -18,7 +19,11 @@ public class MainGame extends JFrame implements Runnable{
 
     private JMenuBar menuBar;
 
-    Board board;
+    private Board board;
+    private boolean[][] grid = new boolean[8][8];
+
+    private static Point initPos = null;
+    private static Point finalPos = null;
 
     private void createMenuBar() {
         menuBar = new JMenuBar();
@@ -40,22 +45,53 @@ public class MainGame extends JFrame implements Runnable{
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        addMouseListener(new MainGameController());
 
-        createMenuBar();
-        setJMenuBar(menuBar);
+//        createMenuBar();
+//        setJMenuBar(menuBar);
 
         setVisible(true);
         init();
     }
 
+    public static void clickTail(Point p)
+    {
+        if(initPos == null)
+            initPos = p;
+        else
+            finalPos = p;
+    }
+
     private void init() {
         board = new Board();
+
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                grid[j][i] = false;
+            }
+        }
+
+
         running = true;
         run();
     }
 
     private void update()
     {
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                grid[j][i] = false;
+            }
+        }
+
+        if(initPos != null)
+            grid[initPos.x][initPos.y] = true;
+        if(finalPos != null)
+            grid[finalPos.x][finalPos.y] = true;
 
     }
 
@@ -69,6 +105,16 @@ public class MainGame extends JFrame implements Runnable{
         Graphics g = bs.getDrawGraphics();
 
         board.draw(g);
+        g.setColor(Color.GREEN);
+
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                if(grid[j][i])
+                    g.drawRect(j*60, (i*60)+30, 60, 60);
+            }
+        }
 
         g.dispose();
         bs.show();
